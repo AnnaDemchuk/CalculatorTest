8226db527bed3a14f8c802052944a57d16037665
Anna Demchuk <anidemchuk@gmail.com>
2020-09-17 16:53:46 +0300

    Add files classes

---
+++ UnitTestProjectMsTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using CalculatorTest;
+using NUnit.Framework.Constraints;
+
+namespace UnitTestProjectMsTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        
+        [Test]
+        public void DivideTwoNumbers()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Divide(6, 3);
+            Assert.AreEqual(2, result);
+        } 
+    }
+}

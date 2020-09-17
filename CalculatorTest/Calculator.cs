d908bae43d27e8790c5651a9dd2cfe039be430a7
AnnaDemchuk <34246748+AnnaDemchuk@users.noreply.github.com>
2020-09-17 12:08:21 +0300

    Add project

---
+++ CalculatorTest/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    public class Calculator
+    {
+        public double Divide(double number1, double number2)
+        {
+            double result = number1 / number2;
+            return result;
+        }
+
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Substract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
